6e6f13c3dd36920ddda59e6a40456530e4379d73
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-02-16 18:52:57 +0300

    Update first_steps.rb

--- first_steps.rb
+++ first_steps.rb
@@ -1,1 +1,5 @@
+puts "Hello World" # print()
+puts "Hello World".reverse # все понятно
+puts "Hello World".length # same
+puts 1987.to_s.reverse # format to string, after reverse, num couldnt be reversed
 
